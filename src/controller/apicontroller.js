58da7f095fb84b159d9c3292bb327cc3197260f7
AlbeiroZerpa <96552743+AlbeiroZerpa@users.noreply.github.com>
2024-03-08 18:25:02 -0400

    commit con servicios chulitos

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,49 +1,54 @@
-const fs = require('fs');
-const myConsole = new console.Console(fs.createWriteStream('./logsgtp.txt'))
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./logsgtp.txt"));
 
 const enviarmensaje = require("../service/apiservice");
 
+const verificar = (req, res) => {
+  try {
+    var tokenalbecode = "ALBECODENODEJSAPIMETA";
+    var token = req.query["hub.verify_token"];
+    var challenge = req.query["hub.challenge"];
 
-const verificar=(req,res)=>{
-
-    try {
-        var tokenalbecode = "ALBECODENODEJSAPIMETA";
-        var token = req.query["hub.verify_token"];
-        var challenge = req.query["hub.challenge"];
-
-        if (challenge != null && token != null && token == tokenalbecode){
-            res.send(challenge);
-        }else{
-            res.status(400).send();
-        }
-        myConsole.log(res);
-    }catch(e){
-        myConsole.log(e);
-        res.status(400).send();
+    if (challenge != null && token != null && token == tokenalbecode) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
     }
-}
-const recibir=(req,res)=>{
-    try {
-        var entry = (req.body["entry"])[0];
-        var changes = (entry["changes"])[0];
-        var value = changes["value"];
-        var objetoMensaje = value["messages"];
+    myConsole.log(res);
+  } catch (e) {
+    myConsole.log(e);
+    res.status(400).send();
+  }
+};
+const recibir = (req, res) => {
+  try {
+    var entry = req.body["entry"][0];
+    var changes = entry["changes"][0];
+    var value = changes["value"];
+    var objetoMensaje;
 
-        var messages = objetoMensaje[0];
-        var number = messages["from"];
-        var texto = messages["text"]["body"];
+    if (value && value["messages"]) {
+      objetoMensaje = value["messages"];
+      var messages = objetoMensaje[0];
+      var texto = messages["text"]["body"];
+      var number = messages["from"];
 
-        myConsole.log("Enviado desde :"+ number);
-        myConsole.log("Mensaje: "+ texto);
-        enviarmensaje.EnviarMensajeWhatsapp(texto, number);
-        
-        res.send("EVENT_RECEIVED");   
-    }catch(e){
-        myConsole.log(e);
-        res.send("EVENT_RECEIVED");        
+      myConsole.log("Mensaje: " + texto);
+      myConsole.log("Enviado desde :" + number);
+      enviarmensaje.EnviarMensajeWhatsapp(texto, number);
+    } else {
     }
-}
+
+    myConsole.log(objetoMensaje);
+
+    res.send("EVENT_RECEIVED");
+  } catch (e) {
+    myConsole.log(e);
+    res.send("EVENT_RECEIVED");
+  }
+};
 
 module.exports = {
-    verificar, recibir
-}
+  verificar,
+  recibir,
+};
